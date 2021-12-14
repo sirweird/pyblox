c750de90a5094a71b0907c7bc7dc2092997921d0
Brendan <65614196+sirweird@users.noreply.github.com>
2021-12-14 16:51:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,15 @@
     <title>pyblox</title>
 </head>
 <body>
+<STYLE>
+.LinearGradient {
+   background: yellow;
+   background: -webkit-linear-gradient(10deg, yellow 40%, #40FF00,blue);
+   background: -o-linear-gradient(10deg, yellow 40%, #40FF00, blue);
+   background: -moz-linear-gradient(10deg 40%, yellow, #40FF00, blue);
+   background: linear-gradient(10deg, yellow 40%, #40FF00, blue);
+}
+</STYLE>
 <h1>welcome to pyblox</h1>
 <h3>this is where to download the app</h3>
 </body>
